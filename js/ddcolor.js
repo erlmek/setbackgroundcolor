e541daae1b6007c2ae72588e5c9a0478ff19d88d
erlmek <erlm@ek.dk>
2025-09-12 13:19:10 +0200

    dropdown select color

--- js/ddcolor.js
+++ js/ddcolor.js
@@ -8,9 +8,14 @@ console.log(ddColor)
 const pbFillDropdown = document.getElementById("pbFillDropdown")
 console.log(pbFillDropdown)
 
+let bdy = document.querySelector("body");
+console.log(bdy);
+
+
 function addColors(btn) {
-    console.log("addcolor")
+    ddColor.innerHTML = ""
     colArray.forEach(fillDropdown)
+    console.log(document.all)
 }
 
 function fillDropdown(item) {
@@ -20,7 +25,13 @@ function fillDropdown(item) {
     ddColor.appendChild(el)
 }
 
+function setBackgroundColor(e) {
+    const selindex = ddColor.selectedIndex
+    const selectedOption = ddColor.options[selindex]
+    const color = selectedOption.innerText;
+    bdy.style.backgroundColor = color
+}
 
 pbFillDropdown.addEventListener("click", addColors)
-
+ddColor.addEventListener("change", setBackgroundColor)
 
