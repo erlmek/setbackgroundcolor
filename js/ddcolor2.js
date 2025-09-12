5ee29c368be0cd3ab192e874eb50f3a689de5fdf
erlmek <erlm@ek.dk>
2025-09-12 14:15:11 +0200

    dropdown nu med objects

--- js/ddcolor2.js
+++ js/ddcolor2.js
@@ -1,6 +1,8 @@
 console.log("jeg er i ddcolor2")
 
 const colArray2 = [["red","#ff0000"], ["green","#00ff00"], ["yellow","#ffff00"], ["purple","#1f35a9"]]
+const colorObjects = [{"name" : "red", "id": "#ff0000"}, {"name" : "green", "id" : "#00ff00"}, {"name" : "yellow", "id" : "#ffff00"}, {"name" : "purple", "id" : "#1f35a9"}]
+
 
 const ddColor2 = document.getElementById("ddColor2")
 console.log(ddColor2)
@@ -16,9 +18,17 @@ function fillDropdown2(item) {
     ddColor2.appendChild(el)
 }
 
+function fillDropdownObj(item) {
+    console.log(item)
+    const el = document.createElement("option")
+    el.textContent = item.name
+    el.value = item.id
+    ddColor2.appendChild(el)
+}
+
 function addColors2(btn) {
     ddColor2.innerHTML = ""
-    colArray2.forEach(fillDropdown2)
+    colorObjects.forEach(fillDropdownObj)
 }
 
 function setBackgroundColor2(e) {
